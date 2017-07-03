cd674ade11f3566544f55ce76ed8cb0cf222f431
asurion <asurion61@gmail.com>
2017-07-03 11:50:58 +0200

    NEW TEMPLATE TA MERE

--- template.php
+++ template.php
@@ -31,22 +31,159 @@
         </script>
         <!-- Piwik -->
 
-		<!-- Load CSS file -->
-		<link href="<?php WEBROOT ?>assets/css/semantic.min.css" media="screen" rel="stylesheet" type="text/css">
+		<!-- Bootstrap core CSS -->
+		<link href="<?php WEBROOT ?>assets/css/bootstrap.min.css" rel="stylesheet">
 
-		<!-- Load JS files -->
-		<script src="<?php WEBROOT ?>assets/js/jquery.min.js" type="text/javascript"></script>
-		<!--[if lt IE 9] -->
-		<script src="<?php WEBROOT ?>assets/js/html5shiv.js" type="text/javascript"></script>
-		<!-- [endif]-->		
-		<script src="<?php WEBROOT ?>assets/js/semantic.min.js"></script>
+		<!-- Custom styles for this template -->
+		<link href="<?php WEBROOT ?>assets/css/custom.css" rel="stylesheet">
+		<link href="<?php WEBROOT ?>assets/css/flexslider.css" rel="stylesheet">			
+
+		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</head>
 	<body>
-	    <header>
-	    	MENU
-	    </header>
+    	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#topWrap">
+						<span class="fa-stack fa-lg">
+							<i class="fa fa-circle fa-stack-2x"></i>
+							<i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+						</span>
+						App<span class="title">Bay</span>
+					</a>
+				</div>
+				<div class="collapse navbar-collapse appiNav">
+					<ul class="nav navbar-nav">
+						<li><a href="#featureWrap">Features</a></li>
+						<li><a href="#productWrap">The App</a></li>
+						<li><a href="#pricingWrap">Pricing</a></li>
+						<li><a href="#testimonialsWrap">Testimonials</a></li>
+						<li><a href="#contactWrap">Contact Us</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
 	    <main>
 	      	<?php echo $content; ?>
 	    </main>
+	    	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<p>Copyright &copy; 2014 AppBay - Responsive App Landing Page Template. Built by <a href="http://bootstrapbay.com">BootstrapBay</a>. All Rights Reserved</p>
+					<p class="social">
+						<a href="https://www.facebook.com/bootstrapbay">
+							<span class="fa-stack fa-lg">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+							</span>
+						</a>
+						<a href="https://twitter.com/bootstrapbay">
+							<span class="fa-stack fa-lg">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+							</span>
+						</a>
+						<a href="https://plus.google.com/+BootstrapbayThemes">
+							<span class="fa-stack fa-lg">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-google-plus fa-stack-1x fa-inverse"></i>
+							</span>
+						</a>
+						<a href="http://www.youtube.com/user/bootstrapbayofficial">
+							<span class="fa-stack fa-lg">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-youtube fa-stack-1x fa-inverse"></i>
+							</span>
+						</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	 <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="<?php WEBROOT ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php WEBROOT ?>assets/js/flexslider.js"></script>
+	
+<script type="text/javascript">
+$(document).ready(function() {
+
+	$('.mobileSlider').flexslider({
+		animation: "slide",
+		slideshowSpeed: 3000,
+		controlNav: false,
+		directionNav: true,
+		prevText: "&#171;",
+		nextText: "&#187;"
+	});
+	$('.flexslider').flexslider({
+		animation: "slide",
+		directionNav: false
+	});
+		
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if ($(window).width() < 768) {
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top - $('.navbar-header').outerHeight(true) + 1
+					}, 1000);
+					return false;
+				}
+			}
+			else {
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top - $('.navbar').outerHeight(true) + 1
+					}, 1000);
+					return false;
+				}
+			}
+
+		}
+	});
+	
+	$('#toTop').click(function() {
+		$('html,body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+	
+	var timer;
+    $(window).bind('scroll',function () {
+        clearTimeout(timer);
+        timer = setTimeout( refresh , 50 );
+    });
+    var refresh = function () {
+		if ($(window).scrollTop()>100) {
+			$(".tagline").fadeTo( "slow", 0 );
+		}
+		else {
+			$(".tagline").fadeTo( "slow", 1 );
+		}
+    };
+		
+});
+</script>
+
 	</body>
 </html>
